b16a991feb9d16eb43766bd88584d3e45b41be0a
Mario Tejada <mariotmbross@hotmail.com>
2024-06-28 10:00:39 -0600

    feat: responsive home page

--- lib/features/landing/views/welcome_section.dart
+++ lib/features/landing/views/welcome_section.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:my_resume_app/core/responsive/responsive_layout.dart';
 import 'package:my_resume_app/features/landing/widgets/welcome/im_mario_body.dart';
 import 'package:my_resume_app/features/landing/widgets/welcome/social_media_icon_list.dart';
 
@@ -16,19 +17,31 @@ class _WelcomeSectionState extends State<WelcomeSection> {
     return const Column(
       children: [
         ImMarioTejadaBody(),
-        Gap(15),
-        Flex(
-          direction: Axis.horizontal,
-          children: [
-            Expanded(
-              child: Center(child: SocialMediaIconList()),
-            ),
-            Expanded(
-                child: Text(
-                    style: TextStyle(fontSize: 24),
-                    textAlign: TextAlign.center,
-                    '“Code never lies, comments sometimes do.”\n-Ron Jeffries'))
-          ],
+        Gap(20),
+        ResponsiveLayout(
+          childMobile: Flex(
+            direction: Axis.vertical,
+            children: [
+              Text(
+                  style: TextStyle(fontSize: 20),
+                  textAlign: TextAlign.center,
+                  '“Code never lies, comments sometimes do.”\n-Ron Jeffries'),
+              Center(child: SocialMediaIconList()),
+            ],
+          ),
+          childDesktop: Flex(
+            direction: Axis.horizontal,
+            children: [
+              Expanded(
+                child: Center(child: SocialMediaIconList()),
+              ),
+              Expanded(
+                  child: Text(
+                      style: TextStyle(fontSize: 24),
+                      textAlign: TextAlign.center,
+                      '“Code never lies, comments sometimes do.”\n-Ron Jeffries'))
+            ],
+          ),
         ),
         Gap(10)
       ],
